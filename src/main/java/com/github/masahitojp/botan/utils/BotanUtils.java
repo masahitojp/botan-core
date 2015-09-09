cf74c87baf73f4187e9c61766f66ef2e69f58ded
masahitojp <randomstep@gmail.com>
2015-09-10 02:27:05 +0900

    modify BotanUtils & bump version

--- src/main/java/com/github/masahitojp/botan/utils/BotanUtils.java
+++ src/main/java/com/github/masahitojp/botan/utils/BotanUtils.java
@@ -1,20 +1,21 @@
 package com.github.masahitojp.botan.utils;
 
-import com.github.masahitojp.botan.message.BotanMessage;
-import com.github.masahitojp.botan.listener.BotanMessageListener;
-import com.github.masahitojp.botan.listener.BotanMessageListenerSetter;
 import lombok.experimental.UtilityClass;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 @UtilityClass
+@SuppressWarnings("unused")
 public final class BotanUtils {
-    public static <T> T getRandom(final List<T> list) {
+
+    public static <T> T getRandomValue(final List<T> list) {
         int index = new Random().nextInt(list.size());
         return list.get(index);
+    }
+
+    public static Optional<String> envToOpt(final String envName) {
+        return Optional.ofNullable(System.getenv(envName));
     }
 }
